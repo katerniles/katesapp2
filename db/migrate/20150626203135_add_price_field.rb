54d25fbaa00db508ee0a7495aee01d8bc9c838e4
Kate Russell <krussellnd@gmail.com>
2015-06-28 09:59:35 -0400

    add precision to price

--- db/migrate/20150626203135_add_price_field.rb
+++ db/migrate/20150626203135_add_price_field.rb
@@ -1,5 +1,5 @@
 class AddPriceField < ActiveRecord::Migration
   def change
-  	add_column :products, :price, :decimal
+  	add_column :products, :price, :decimal, :precision => 8, :scale => 2
   end
 end
