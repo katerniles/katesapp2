96efa5de40298a9c04fd877fcef01ceb3827271d
Kate Russell <krussellnd@gmail.com>
2015-07-05 11:34:33 -0400

    updated landing page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @product = Product.all
+    @products = Product.all
     render layout: "products"
   end
 
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def landing_page
-  	@products = Product.limit(3)
+  	@featured_product = Product.first
+#  	@products = Product.limit(3)
   end
 end
