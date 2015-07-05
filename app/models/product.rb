9131c66d001f56971fd632e2eb4f52065719c194
Kate Russell <krussellnd@gmail.com>
2015-07-05 11:07:29 -0400

    work on products page

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,12 @@ class Product < ActiveRecord::Base
 	has_many :orders
 	has_many :comments
 	validates :name, presence: true
+	validates :price_in_cents, presence: true
+
 	def average_rating
 		comments.average(:rating).to_f
-end
+	end
+	
 	def price_in_cents
 		(self.price*100).to_i
 	end
