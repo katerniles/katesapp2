67a1113fee853697d8f4911dd2e5f3d21c1e3764
Kate Russell <krussellnd@gmail.com>
2015-07-05 11:17:43 -0400

    fixed product def?

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @product = Product.all
     render layout: "products"
   end
 
