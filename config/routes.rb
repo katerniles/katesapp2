8a57150b2d37ed33bc4afe2442888a4148b40f7e
Kate Russell <krussellnd@gmail.com>
2015-07-13 18:02:56 -0400

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   get 'static_pages/index'
+  
+  get 'static_pages/landing_page'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
