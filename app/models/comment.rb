bcbe3765b2beafb339544f82e3cdd28576de5591
Kate Russell <krussellnd@gmail.com>
2015-07-28 13:52:31 -0400

    create comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
-  validates :body, presence: true
-  validates :rating, presence: true, numericality: {  only_integer: true  }
 end
