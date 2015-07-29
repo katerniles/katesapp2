7d382791e5c3b0c890b7f031f7c2ee410b4f76de
Kate Russell <krussellnd@gmail.com>
2015-07-29 15:28:59 -0400

    still working on comments

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-
+gem 'cancancan'
 gem 'devise'
 ruby '2.2.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- db/migrate/20150729183023_add_admin_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddAdminToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :admin, :string
-    add_column :users, :boolean, :string
-  end
-end
