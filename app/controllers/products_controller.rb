e8c3bd3a4dfdc556df28d0d686ad0c28a07332b6
Kate Russell <krussellnd@gmail.com>
2015-08-09 19:49:00 -0400

    cleanup of views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
   def index
     if params[:q]
       search_term = params[:q] 
-      @products = Product.search(search_term)
+      @products = Product.where("name LIKE ?", "%#{search_term}%")
     else
       @products = Product.all
     end
