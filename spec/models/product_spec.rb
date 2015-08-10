8eccbbcc988baad2bf032f43a994f4bf09a17898
Kate Russell <krussellnd@gmail.com>
2015-08-10 17:28:48 -0400

    work of rspecs and install factory girl

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,67 +1,13 @@
 require 'rails_helper'
 
-describe Product do 
-  
-  describe '#name' do
-    context 'when missing' do
-      before do
-        expect(subject.name).to be_blank 
-      end  
-
-      it 'renders subject invalid' do
-        subject.valid?
-        expect(subject.errors[:name]).to include("can't be blank")
-      end 
+RSpec.describe Product, type: :model do 
+  before do
+    @product = Product.create(name: "Tree")
     end
-    
-    context 'when present' do
-      subject { build(:product) }
-      before do
-        expect(subject.name).to be_present
-      end 
 
-      it 'does not have errors on name' do
-        subject.valid?
-        expect(subject.errors[:name]).to be_blank        
-      end  
-    end  
-  end # name
+    subject { @product }
 
-  describe '#average_rating' do
-    context 'when we have 5 comments with ratings' do
-      subject { build(:product) }
-      before do
-        # user = User.new(email: "foo@mailinator.com", password: '12345678aE')
-        # user.save!
-        user = create :user
-         # build is equivalent to .new
-         # create is equivalent to .new and then save
-        subject.comments << build(:comment, rating: 1, user: user)
-        subject.comments << build(:comment, rating: 2, user: user)
-        subject.comments << build(:comment, rating: 3, user: user)
-        subject.comments << build(:comment, rating: 4, user: user)
-        subject.comments << build(:comment, rating: 5, user: user)
-        subject.save!
-      end  
-
-      it 'returns 3' do
-        expect(subject.average_rating).to eq(3)
-      end  
-    end  
-
-  end  
-    
-   describe '#comments' do
-    it 'can have comments' do
-      expect(subject).to respond_to(:comments)
-    end  
-  end  
- 
-   describe Product, 'validation' do
-    subject { build(:product) }
-    it { should validate_uniqueness_of(:key) }
-    it { should validate_uniqueness_of(:name) }
-    it { should validate_presence_of(:key) }
-  end 
-
+    it "should return name" do
+        expect(@product.name).to eq "Tree"
+      end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,39 +1,16 @@
 require 'rails_helper'
+require 'spec_helper'
 
-  describe User do
 
-  context "first and last name present" do
-
-    before { @user = User.new(first_name: "Kate", last_name: "Russell") }
-
-    it "should return first and last"
-      expect( @user.full_name).to eq "Kate Russell"
+RSpec.describe User, type: :model do
+    before do 
+      @user = User.create(first_name: "Kate", last_name: "Russell")
     end
-  end
 
-  context "first name missing" do
+    subject { @user }
 
-    before { @user = User.new(last_name: "Russell") }
-
-    it "should return last name only" do
-      expect( @user.full_name).to eq "Russell"
-    end
-  end
-
-  context "last name missing" do
-
-    before { @user = User.new(first_name: "Kate") }
-
-    it "should return first name only" do
-      expect( @user.full_name).to eq "Kate"
-    end
-  end
-
-  context "no names present" do
-
-    before { @user = User.new() }
-
-    it  "should return nothing" do
-      expect( @user.full_name).to eq ""
-    end
-  end
+    it "should return first and last" do
+        expect(@user.first_name).to eq "Kate"
+        expect(@user.last_name).to eq "Russell"
+      end
+end
