37af397aa0b82457df5342d5297a903d67da9d16
Kate Russell <krussellnd@gmail.com>
2015-08-15 17:35:46 -0400

    cookie store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, 
-	key: '_katesapp_session'
-	expire_after: 2.minutes
+Rails.application.config.session_store :cookie_store, {
+	:key => 	'_session_id',
+	:secure => 	Rails.env.production?
 end
