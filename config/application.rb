bc3975bbd818fc6dcf5a4a6cb7f4d15759d9b0b1
Kate Russell <krussellnd@gmail.com>
2015-08-17 18:57:58 -0400

    ssl off for development

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Katesapp
   class Application < Rails::Application
+    config.fore_ssl = false
+  end
+end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,8 +44,9 @@ Rails.application.configure do
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
-
+  katesapp::Application.configure do
+    config.force_ssl = true
+  end
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
