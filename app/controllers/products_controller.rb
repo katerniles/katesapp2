15df19c8ead3e50df79ac3069c4aeda85e3afc57
Kate Russell <krussellnd@gmail.com>
2015-08-18 12:42:43 -0400

    refix user ability to see prods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :edit, :update, :destroy]
+
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!
   # GET /products
   # GET /products.json
   def index
